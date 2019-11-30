89931fd7501a0003768a4d9d7a3079ffbea67f2c
David van Balen <dpvanbalen@hotmail.com>
2019-11-30 19:43:36 +0100

    update

--- src/AccPrune.hs
+++ src/AccPrune.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RebindableSyntax, ScopedTypeVariables, ViewPatterns, TypeApplications, TypeOperators, FlexibleContexts #-}
+{-# LANGUAGE RebindableSyntax, ScopedTypeVariables, ViewPatterns, TypeOperators, FlexibleContexts #-}
 module AccPrune where
 import Utils
 import Data.Array.Accelerate as A
@@ -6,25 +6,25 @@ import Data.Word
 import Data.Array.Accelerate.Data.Bits
 import Data.Array.Accelerate.Control.Lens
 import qualified Prelude as P
-  
+
 type Cell = Word16 --bitmap, bits 0 to 8 are numbers and bit 9 represents that there is only one option left.
- 
+
 
 -- Returns the pruned list of sudokus, together with a list of bools representing whether the sudoku is inconsistent (contains a field that can't be filled anymore)
--- TODO check whether this 'check' is sufficient, or we also need a check that there are no 2 identical singles in a line/column/block.
 pruneAndCheck :: Acc (Array DIM3 Cell) -> Acc (Array DIM3 Cell, Array DIM1 Bool) --n*9*9 -> (n*9*9,n)
 pruneAndCheck = finish . pruneGrids where
   finish :: Acc (Array DIM3 Cell) -> Acc (Array DIM3 Cell, Array DIM1 Bool)
-  finish xs = lift (xs, zipWith (&&) (check1 xs) (check2 xs))
+  finish xs = lift (xs, zipWith3 (\x y z -> x&&y&&z) (check1 xs) (check2 xs) (check3 xs))
   check1, check2 :: Acc (Array DIM3 Cell) -> Acc (Array DIM1 Bool)
   check1 = fold1 (&&) . fold1 (&&) . map (/=0)
   check2 = fold1 (&&) . map (\x -> 511 == x.&.511) . fold1 (.|.)
+  check3 = check2 . transposeOn _1 _2
 
 {- gets a list of sudokus (2+1=3dim) (n*9*9)
  -
  - replicate this 3 times (4dim) (3*n*9*9)
  - permute the second one to make it columns instead of rows
- - permute the third one to make it blocks instead of rows 
+ - permute the third one to make it blocks instead of rows
  -
  - replicate this twice, applying 'weirdtransform' to one of them. (2*3*n*9*9)
  - weirdtransform applies a bijection between the numbers 1..9 and the 9 fields in each `row`.
@@ -40,25 +40,25 @@ pruneAndCheck = finish . pruneGrids where
  -
  - map over everything to set all relevant bit9s
  -
- - use awhile to loop this 
+ - use awhile to loop this
  - -}
 
 -- pruneGrids is `safe` on sudokus that are still possible, but as soon as a bad guess is done outside of pruneGrid it will ofcourse result in questionable sudoku solutions. This needs to be checked seperately.
-pruneGrids :: Acc (Array DIM3 Cell) -> Acc (Array DIM3 Cell) 
-pruneGrids xs = mySnd $ awhile cond step ((lift :: Arrays a => (Acc a, Acc a) -> Acc (a,a)) (xs, (pruneGrids' xs)))
- where 
+pruneGrids :: Acc (Array DIM3 Cell) -> Acc (Array DIM3 Cell)
+pruneGrids xs = mySnd $ awhile cond step ((lift :: Arrays a => (Acc a, Acc a) -> Acc (a,a)) (xs, pruneGrids' xs))
+ where
   myFst, mySnd :: forall a. Arrays a => Acc (a, a) -> Acc a
   myFst t = let (x, y) = unlift t :: (Acc a, Acc a) in x
   mySnd t = let (x,y) = unlift t :: (Acc a, Acc a) in y
   step :: Acc (Array DIM3 Cell, Array DIM3 Cell) -> Acc (Array DIM3 Cell, Array DIM3 Cell)
   step t = let (x,y) = unlift t :: (Acc (Array DIM3 Cell), Acc (Array DIM3 Cell)) in lift (y, pruneGrids' y)
-  cond x = map not $ foldAll (&&) (constant True) . map (\y -> fst y == snd y) $ zip (myFst x) (mySnd x)
+  cond x = map not $ foldAll (&&) (constant True) $ zipWith (==) (myFst x) (mySnd x)
 
 
 pruneGrids' :: Acc (Array DIM3 Cell) -> Acc (Array DIM3 Cell)
-pruneGrids' = mapSingles . fuseSudokus . pruneDoubles . pruneSingles . mapSingles . splitSudokus 
+pruneGrids' = mapSingles . fuseSudokus . pruneDoubles . pruneSingles . mapSingles . splitSudokus
 
-splitSudokus = weirdtransform . (\xs -> backpermute (shape xs) permutation xs) . (replicate (constant (Z:.i3:.All:.All:.All))) . map (`clearBit` 9) 
+splitSudokus = weirdtransform . (\xs -> backpermute (shape xs) permutation xs) . replicate (constant (Z:.i3:.All:.All:.All)) . map (`clearBit` 9)
 
 permutation :: Exp DIM4 -> Exp DIM4 -- this permutation is a bijection, and its own inverse.
 permutation (unindex4 -> (m, n, i, j)) =    if m == 0 then lift $ Z:.0:.n:.i:.j                          -- rows
@@ -68,50 +68,49 @@ permutation (unindex4 -> (m, n, i, j)) =    if m == 0 then lift $ Z:.0:.n:.i:.j 
 
 rep1 = replicate (constant (Z:.All:.All:.i1:.All:.All))
 weirdtransform xs = concatOn _3 (rep1 xs) (rep1 $ weirdtransform' xs) -- adds another layer, bijecting between numbers and fields.
-unweirdtransform xs = let ys = slice xs (constant (Z:.All:.All:.i0:.All:.All)); 
-                          zs = slice xs (constant (Z:.All:.All:.i1:.All:.All)) in 
+unweirdtransform xs = let ys = slice xs (constant (Z:.All:.All:.i0:.All:.All));
+                          zs = slice xs (constant (Z:.All:.All:.i1:.All:.All)) in
    zipWith (.&.) ys (weirdtransform' zs)
-weirdtransform' :: Acc (Array DIM4 Cell) -> Acc (Array DIM4 Cell) 
-weirdtransform' xs = let (ones,twos,threes,fours,fives,sixes,sevens,eights,nines) = (f 1,f 2,f 4,f 8,f 16,f 32,f 64,f 128,f 256); --weirdtransform' is also its own inverse, except for bit9
-                         f (x :: Exp Word16) = replicate (constant (Z:.All:.All:.All:.i1)) . fold1 (.|.) . A.imap (\(unindex4 -> (_,_,_,i)) y -> if x.&.y == 0 then 0 else setBit 0 i) $ xs;
+
+--weirdtransform' is also its own inverse, except for bit9
+weirdtransform' :: Acc (Array DIM4 Cell) -> Acc (Array DIM4 Cell)
+weirdtransform' xs = let (ones,twos,threes,fours,fives,sixes,sevens,eights,nines) = (f 1,f 2,f 4,f 8,f 16,f 32,f 64,f 128,f 256);
+                         f x = replicate (constant (Z:.All:.All:.All:.i1)) . fold1 (.|.) . A.imap (\(unindex4 -> (_,_,_,i)) y -> if x.&.y == 0 then 0 else setBit 0 i) $ xs;
                            in ones ++ twos ++ threes ++ fours ++ fives ++ sixes ++ sevens ++ eights ++ nines
-                 
+
 
 pruneSingles xs = let ys = replicate (constant (Z:.All:.All:.All:.All:.i9)) (findSingles xs) in zipWith (\x y -> if testBit x 9 then x else x .&. (complement y)) xs ys
 
 -- bit 9 represents a field having only 1 option left, here we .|. all those bits.
-findSingles = map (`clearBit` 9) . fold1 (.|.) . map (\x -> if testBit x 9 then x else 0)
+findSingles = fold1 (.|.) . map (\x -> if testBit x 9 then x `clearBit` 9 else 0)
 
 fuseSudokus = fold1 (.&.) . transposeOn _1 _4 . transposeOn _1 _3 . transposeOn _1 _2 . (\xs -> backpermute (shape xs) permutation xs) . unweirdtransform
 
 mapSingles  :: Shape a => Acc (Array a Cell) -> Acc (Array a Cell)
 mapSingles  = map (\x -> if popCount x == 1 then setBit x 9 else x) . map (`clearBit` 9)
 
-pruneDoubles = (\xs -> let ys = replicate (constant (Z:.All:.All:.All:.All:.i9)) (findDoubles xs) in 
-              zipWith (\x y -> if popCount x <= 2 {-&& x==x.&.y-} then x else x.&.(complement y)) xs ys) . map (`clearBit` 9) 
-                 -- part commented out would be a speedup, but only safe if unDoubleIndexes could find all.
+pruneDoubles = (\xs -> let ys = replicate (constant (Z:.All:.All:.All:.All:.i9)) (findDoubles xs) in
+              zipWith (\x y -> if popCount x <= 2 {-&& x==x.&.y-} then x else x.&.complement y) xs ys) . map (`clearBit` 9)
 
-findDoubles = map unDoubleIndexes . map (\(unlift -> (z1,z2,z3)::(Exp Word64, Exp Word64, Exp Word64)) -> z2 - z3) . fold1 (\(unlift -> (x1,x2,x3)) (unlift -> (y1,y2,y3)) -> 
+findDoubles = map unDoubleIndexes . map (\(unlift -> (z1,z2,z3)::(Exp Word64, Exp Word64, Exp Word64)) -> z2 - z3) . fold1 (\(unlift -> (x1,x2,x3)) (unlift -> (y1,y2,y3)) ->
                  lift (x1.|.y1, x2.|.y2.|.(x1.&.y1), x3.|.y3.|.(x1.&.y2).|.(x2.&.y1))) --(seen at least once, seen at least twice, seen more times)
                    . map (\x -> lift (x,0,0) :: Exp (Word64, Word64, Word64)) . map doubleIndexes
 
 
--- It seems that this always hangs if uncommented, is recursion inside the Exp world not allowed? 
--- I had hoped that, once all `threads` would match the ==0 condition, this would return. Now it can only do one pair per line at a time.
 unDoubleIndexes :: Exp Word64 -> Exp Word16 -- inverse of below, for a map containing an arbitrary number of bits that each refer to two numbers, recover which numbers are all relevant
-unDoubleIndexes x = if x == 0 then 0 else (fromIntegral $ unDoubleIndex y)-- .|. (fromIntegral $ unDoubleIndexes (clearBit x y))
- where
+unDoubleIndexes x = fst $ while ((/=0) . snd) (\(unlift -> (a:: Exp Word16, b :: Exp Word64)) -> lift ((a .|.) .fromIntegral . unDoubleIndex $ b, clearBit b (countTrailingZeros b))) $ lift (0,x)
+  where
   --unDoubleIndex could also be written with the floor of a square root for less conditionals
-  unDoubleIndex :: Exp Int -> Exp Word64
-  unDoubleIndex i = if i< 8 then setBit (shift x    1 ) 0 else
-                    if i<15 then setBit (shift x (- 6)) 1 else
-                    if i<21 then setBit (shift x (-12)) 2 else
-                    if i<26 then setBit (shift x (-17)) 3 else
-                    if i<30 then setBit (shift x (-21)) 4 else
-                    if i<33 then setBit (shift x (-24)) 5 else
-                    if i<35 then setBit (shift x (-26)) 6 else
-                                  setBit (setBit 0 8)    7
-  y = countTrailingZeros x
+  unDoubleIndex :: Exp Word64 -> Exp Word64
+  unDoubleIndex x = let i = countTrailingZeros x in
+          if i <  8 then setBit (shift x    1 ) 0 else
+          if i < 15 then setBit (shift x (- 6)) 1 else
+          if i < 21 then setBit (shift x (-12)) 2 else
+          if i < 26 then setBit (shift x (-17)) 3 else
+          if i < 30 then setBit (shift x (-21)) 4 else
+          if i < 33 then setBit (shift x (-24)) 5 else
+          if i < 35 then setBit (shift x (-26)) 6 else
+                         setBit (setBit 0 8)    7
 
 
 
@@ -119,7 +118,7 @@ doubleIndexes :: Exp Word16 -> Exp Word64 --given a word, if it has exactly two 
 doubleIndexes i = if popCount i /= 2 then 0 else setBit 0 $ secondBit + (36 - shift ((9-firstBit)*(8-firstBit)) (-1))
  where
   firstBit = countTrailingZeros i
-  secondBit = countTrailingZeros (shift i (-firstBit-1)) 
+  secondBit = countTrailingZeros (shift i (-firstBit-1))
 
 
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,40 +1,64 @@
 module Main where
-import Data.Word
-import Data.Bits
-import Data.Maybe
-import Data.List
-import Utils
-import AccPrune
-import qualified Data.Array.Accelerate as A
-import qualified Data.Array.Accelerate.LLVM.Native as Native
-import qualified Data.Array.Accelerate.Interpreter as Interpreter
-import qualified Data.Array.Accelerate.LLVM.PTX as PTX
+import           Control.Monad
+import           Data.Word
+import           Data.Bits
+import           Data.Maybe
+import           Data.List
+import           Utils
+import           AccPrune
+import qualified Data.Array.Accelerate         as A
+import qualified Data.Array.Accelerate.LLVM.Native
+                                               as Native
+import qualified Data.Array.Accelerate.Interpreter
+                                               as Interpreter
+import qualified Data.Array.Accelerate.LLVM.PTX
+                                               as PTX
 
-main = interact $ pretty . solve . readSudoku
-
+main =
+  interact
+    $ show
+    . (\x -> (length x, map length x, sum $ map length x))
+    . solve
+    . readSudoku
 
 pretty :: [[Word16]] -> String
-pretty [] = "mwep"
-pretty grs = concatMap (\gr -> foldl f "\n" (sud gr)) grs where
-  sud [] = [] 
+pretty []  = "mwep"
+pretty grs = concatMap (foldl f "\n" . sud) grs where
+  sud [] = []
   sud xs = take 9 xs : sud (drop 9 xs)
-  f s xs = s ++ intercalate " " (map (show . unBitmap) xs) ++ "\n"
+  f s xs = s ++ unwords (map (show . unBitmap) xs) ++ "\n"
+
 
 -- currently returns a list of possible solutions, in practice this list usually contains only the actual solution. It should not be hard to filter the bad solutions out.
-solve :: [Word16] -> [[Word16]]
-solve gr = solve' (accelerateStep [gr],[])
-  where
-    solve' :: ([[Word16]],[[Word16]]) -> [[Word16]]
-    solve' ([],xs) = xs
-    solve' (todos,xs) = let ys = accelerateStep . concat . map doGuess $ filter (not . solved) todos in solve' (ys,xs ++ filter solved todos)
-    doGuess :: [Word16] -> [[Word16]]
-    doGuess xs = let i = splitIndex xs in map (\j -> take i xs ++ j : drop (i+1) xs) (splitBits (xs !! i))
-    splitIndex :: [Word16] -> Int
-    splitIndex xs = let ys = map (\x -> if testBit x 9 then 100 else popCount x) xs in fromJust $ elemIndex (minimum ys) ys 
-    accelerateStep :: [[Word16]] -> [[Word16]]
-    accelerateStep xs = let (ys, bools) = accelerateStep' xs in (map snd . filter fst . zip (A.toList bools) . chunkList 81 . A.toList) $ ys
-    accelerateStep' :: [[Word16]] -> (A.Array A.DIM3 Word16, A.Array A.DIM1 Bool)
-    accelerateStep' xs = PTX.run $ pruneAndCheck $ A.use $ A.fromList (A.Z A.:. length xs A.:. 9 A.:. 9) $ concat xs
+solve :: [[Word16]] -> [[[Word16]]]
+solve grs = solve' (accelerateStep grs, [])
+ where
+  solve' :: ([[Word16]], [[[Word16]]]) -> [[[Word16]]]
+  solve' ([], xs) = xs
+  solve' (todos, xs) =
+    let (finished, newtodos) = partition solved todos
+    in  let ys = accelerateStep . concatMap doGuess $ newtodos
+        in  solve' (ys, finished : xs)
+  doGuess :: [Word16] -> [[Word16]]
+  doGuess xs =
+    let i = splitIndex xs
+    in  map (\j -> take i xs ++ j : drop (i + 1) xs) (splitBits (xs !! i))
+  splitIndex :: [Word16] -> Int
+  splitIndex xs =
+    let ys = map (\x -> if testBit x 9 then 100 else popCount x) xs
+    in  fromJust $ elemIndex (minimum ys) ys
+  accelerateStep :: [[Word16]] -> [[Word16]]
+  accelerateStep xs =
+    let (ys, bools) = accelerateStep' xs
+    in  (map snd . filter fst . zip (A.toList bools) . chunkList 81 . A.toList)
+          ys
+  accelerateStep' :: [[Word16]] -> (A.Array A.DIM3 Word16, A.Array A.DIM1 Bool)
+  accelerateStep' xs =
+    PTX.run
+      $ pruneAndCheck
+      $ A.use
+      $ A.fromList (A.Z A.:. length xs A.:. 9 A.:. 9)
+      $ concat xs
 
 solved :: [Word16] -> Bool
 solved = all isOne
@@ -42,35 +66,38 @@ isOne x = testBit x 9
 
 help :: [[Word16]] -> String
 help = show . map (map (map help') . chunkList 9)
-help' x = map (\i -> testBit x (i-1)) [1..9]
+help' x = map (\i -> testBit x (i - 1)) [1 .. 9]
 
-readSudoku :: String -> [Word16]
-readSudoku = take 81 . map f where
+readSudoku :: String -> [[Word16]]
+readSudoku = go . map f . filter valid where
   f '.' = 511 --decimal for the binary value of 9 ones
-  f '1' = (512 +) $ setBit 0 0 
-  f '2' = (512 +) $ setBit 0 1 
-  f '3' = (512 +) $ setBit 0 2 
-  f '4' = (512 +) $ setBit 0 3 
-  f '5' = (512 +) $ setBit 0 4 
-  f '6' = (512 +) $ setBit 0 5 
-  f '7' = (512 +) $ setBit 0 6 
-  f '8' = (512 +) $ setBit 0 7 
-  f '9' = (512 +) $ setBit 0 8 
+  f '1' = (512 +) $ setBit 0 0
+  f '2' = (512 +) $ setBit 0 1
+  f '3' = (512 +) $ setBit 0 2
+  f '4' = (512 +) $ setBit 0 3
+  f '5' = (512 +) $ setBit 0 4
+  f '6' = (512 +) $ setBit 0 5
+  f '7' = (512 +) $ setBit 0 6
+  f '8' = (512 +) $ setBit 0 7
+  f '9' = (512 +) $ setBit 0 8
+  valid x = x `elem` ".123456789"
+  go [] = []
+  go xs = take 81 xs : go (drop 81 xs)
 
 splitBits :: Word16 -> [Word16]
-splitBits w = mapMaybe f [0..8] 
-  where f i
-          | testBit w i = Just $ bit i
-          | otherwise = Nothing
+splitBits w = mapMaybe f [0 .. 8]
+ where
+  f i | testBit w i = Just $ bit i
+      | otherwise   = Nothing
 
 
 unBitmap 513 = 1
 unBitmap 514 = 2
 unBitmap 516 = 3
-unBitmap 520 = 4 
+unBitmap 520 = 4
 unBitmap 528 = 5
 unBitmap 544 = 6
 unBitmap 576 = 7
 unBitmap 640 = 8
 unBitmap 768 = 9
-unBitmap i = 0
+unBitmap i   = 0
